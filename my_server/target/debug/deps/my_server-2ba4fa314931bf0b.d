865db481628637c0f36cf1eb8b82237d830448ee
imohtase <27la.brhoom@gmail.com>
2025-07-04 15:11:53 +0300

    first commit

---
+++ my_server/target/debug/deps/my_server-2ba4fa314931bf0b.d
@@ -0,0 +1,5 @@
+C:\Users\ibraheem\Desktop\hello_rust_shere\my_server\target\debug\deps\libmy_server-2ba4fa314931bf0b.rmeta: src\main.rs
+
+C:\Users\ibraheem\Desktop\hello_rust_shere\my_server\target\debug\deps\my_server-2ba4fa314931bf0b.d: src\main.rs
+
+src\main.rs:
---
+++ my_server/target/debug/deps/my_server-39eb132651314e41.d
@@ -0,0 +1,5 @@
+C:\Users\ibraheem\Desktop\hello_rust_shere\my_server\target\debug\deps\libmy_server-39eb132651314e41.rmeta: src\main.rs
+
+C:\Users\ibraheem\Desktop\hello_rust_shere\my_server\target\debug\deps\my_server-39eb132651314e41.d: src\main.rs
+
+src\main.rs:
